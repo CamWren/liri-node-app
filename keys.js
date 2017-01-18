3ecbaee3d5ecf7d46b14da98f6a062e6500bad95
Cameron Wren <wrnecmrn@gmail.com>
2017-01-18 16:46:50 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-// console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'u9LbPrDA9SHKxlUz48uNxNPQ3',
-  consumer_secret: 'eMIiPjYoohYiciwT1zBK7z6g051CGFPnZqh3FBFvxrcQZoQaYl',
-  access_token_key: '788185284110196736-lmmdxDb52h283Wt95n1T1O2vznBxPdA',
-  access_token_secret: 'rgkPLHNBmBxBhOWHDySdYIg2EHmk6EUt4u5U6fbQuY9xa'
-}
